4dd8e215b1bdfb74e757e8a4679b0c8ce2fc1fe1
Jeffrey Home Laptop <jmcda001@github.com>
2016-12-19 15:32:05 -0800

    Initializing the Cartridge library (Cartridges are entity files)

---
+++ setupCartridgeList.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+listName='.library_list.json'
+folder='Library'
+if [ $# -gt 0 ]
+then
+    folder=$1;
+fi
+
+technologyList='{"libraries": [\n'
+for technology in $folder/*; do
+    technologyList=$technologyList'\t{"path": "'$technology'/",\n'
+    technologyList=$technologyList'\t"name": "'$(basename $technology)'",\n'
+    components='"\tcomponents": [\n'
+    for cartridge in $technology/*; do
+        filename=$(basename $cartridge)
+        components=$components'\t\t{"name": "'${filename%.*}'",\n'
+        components=$components'\t\t"file": "'$filename'"},\n'
+    done;
+    components=${components%???}']\n\t},\n'
+    technologyList=$technologyList$components
+done;
+technologyList=${technologyList%???}']\n}'
+echo $technologyList > $listName
